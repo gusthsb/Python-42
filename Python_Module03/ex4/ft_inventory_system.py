389ba4ea7b2fe775bbd6269d5e7f9058c5adc3c7
gusthsb <gustavo.hdsbrito@gmail.com>
2026-08-27 13:05:22 -0300

    ex4

--- Python_Module03/ex4/ft_inventory_system.py
+++ Python_Module03/ex4/ft_inventory_system.py
@@ -2,7 +2,7 @@
 import sys
 
 
-def add_item(inventory: dict) -> dict:
+def add_item(inventory: dict[str, int]) -> dict[str, int]:
     args = sys.argv[1:]
     for item in args:
         if ":" in item:
@@ -15,24 +15,43 @@ def add_item(inventory: dict) -> dict:
             try:
                 inventory[key] = int(value)
             except ValueError as ve:
-                print(f"Quantity error for {key}: {ve}")
+                print(f"Quantity error for '{key}': {ve}")
                 continue
         else:
             print(f"Error - invalid parameter '{item}'")
             continue
-            
+
     return inventory
 
 
 def show_inventory() -> None:
-    empty_inventory = dict()
+    empty_inventory: dict[str, int] = dict()
     print("=== Inventory System Analysis ===")
     inventory = add_item(empty_inventory)
     print(f"Got inventory: {inventory}")
-    print(f"item list: {list(inventory.keys())}")
-    total_itens = sum(inventory.values())
-    print(f"Total quantity of the {len(inventory.values())} items: {total_itens}")
+    print(f"Item list: {list(inventory.keys())}")
+    if inventory:
+        total_itens = sum(inventory.values())
+        print(f"Total quantity of the {len(inventory.values())}"
+              f" items: {total_itens}")
+        for key in inventory:
+            value = inventory[key]
+            percent = round((value / total_itens) * 100, 1)
+            print(f"item {key} represents {percent}%")
+        most_abundant = list(inventory.keys())[0]
+        least_abundant = list(inventory.keys())[0]
+        for key in inventory:
+            if inventory[key] > inventory[most_abundant]:
+                most_abundant = key
+            if inventory[key] < inventory[least_abundant]:
+                least_abundant = key
+        print(f"Item most abundant: {most_abundant} with quantity"
+              f" {inventory[most_abundant]}")
+        print(f"Item least abundant: {least_abundant} with quantity"
+              f" {inventory[least_abundant]}")
+    inventory.update({"magic_item": 1})
+    print(f"Updated inventory: {inventory}")
 
- 
+
 if __name__ == "__main__":
-    show_inventory()    
+    show_inventory()
