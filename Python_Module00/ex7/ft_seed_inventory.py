f96ec2bf06e8c3d3d0ff191b19602db92c025b92
Gustavo Henrique De Souza Brito <gustde-s@c3r10p1.42sp.org.br>
2026-09-11 18:01:43 -0300

    fix: print packets

--- Python_Module00/ex7/ft_seed_inventory.py
+++ Python_Module00/ex7/ft_seed_inventory.py
@@ -1,6 +1,6 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if unit == "packets":
-        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} avaliable")
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} available")
     elif unit == "grams":
         print(f"{seed_type.capitalize()} seeds: {quantity} {unit} total")
     elif unit == "area":
