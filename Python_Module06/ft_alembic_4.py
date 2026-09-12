acc253335156e4c249cb73a1728d2b1bdc2dc020
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-12 04:19:29 -0300

    fix: import

--- Python_Module06/ft_alembic_4.py
+++ Python_Module06/ft_alembic_4.py
@@ -3,6 +3,6 @@ import alchemy
 
 def ft_alembic_4() -> None:
     print("=== Alembic 4 ===")
-    print(f"Testing create_air: {alchemy.elements.create_air()}")
+    print(f"Testing create_air: {alchemy.create_air()}")
     print("Now show that not all functions can be reached")
     print("This will raise an exception!")
