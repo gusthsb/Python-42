2dd40d124d6fd3073766cad184e4f19ad739f4b5
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-14 19:58:28 -0300

    concluindo transmutation

--- Python_Module06/ft_transmutation_0.py
+++ Python_Module06/ft_transmutation_0.py
@@ -1,7 +1,7 @@
-import alchemy.transmutation.recipes as atr
+import alchemy.transmutation.recipes
 
 
 def ft_transmutation_0() -> None:
     print("=== Transmutation 0 ===")
     print("Using file alchemy/transmutation/recipes.py directly")
-    print(f"Testing lead to gold: {atr.lead_to_gold()}")
+    print(f"Testing lead to gold: {alchemy.transmutation.recipes.lead_to_gold()}")
---
+++ Python_Module06/ft_transmutation_2.py
@@ -0,0 +1,7 @@
+import alchemy
+
+
+def ft_transmutation_2() -> None:
+    print("=== Transmutation 2 ===")
+    print("Import alchemy module only")
+    print(f"Testing lead to gold: {alchemy.transmutation.lead_to_gold()}")
