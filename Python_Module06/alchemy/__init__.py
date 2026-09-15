9de4157ae760cc6eca4c4d06bd4a2967c82c1c67
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-15 01:19:21 -0300

    .

--- Python_Module06/alchemy/__init__.py
+++ Python_Module06/alchemy/__init__.py
@@ -1,3 +1,4 @@
 from .elements import create_air
 from .potions import strength_potion, healing_potion as heal
 from . import transmutation
+from . import grimoire
--- Python_Module06/ft_kaboom_0.py
+++ Python_Module06/ft_kaboom_0.py
@@ -1,7 +1,12 @@
-from alchemy import grimoire
+import alchemy.grimoire
 
 
 def ft_kaboom_0() -> None:
     print("=== Kaboom 0 ===")
     print("Using grimoire module directly")
-    print(f"Testing record light spell: ")
+    print(f"Testing record light spell: "
+          f"{alchemy.grimoire.light_spell_record('Fantasy', 'Earth, wind and fire')}")
+
+
+if __name__ == "__main__":
+    ft_kaboom_0()
