44712c7a54a9be1443dc6c64f7420026a5adf272
Gustavo Henrique De Souza Brito <gustde-s@c1r4p3.42sp.org.br>
2026-09-16 19:39:18 -0300

    .

---
+++ Python_Module06/ft_kaboom_1.py
@@ -0,0 +1,10 @@
+from alchemy.grimoire.dark_spellbook import dark_spell_record
+from .dark_validator import validate_ingredients
+
+
+def ft_kaboom_1() -> None:
+    print("=== Kaboom 1 ===")
+    print("Acess to alchemy/grimoire/dark_spellbook.py directly")
+    print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
+    
+
