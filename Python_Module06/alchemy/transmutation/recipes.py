3a10b787613d78e055e34c5ecbf47af98b010f1c
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-17 02:33:17 -0300

    .

--- Python_Module06/alchemy/transmutation/recipes.py
+++ Python_Module06/alchemy/transmutation/recipes.py
@@ -5,7 +5,7 @@ from ..elements import create_air
 
 def lead_to_gold() -> str:
     return (
-      f"Recipe trasmuting Lead to gold: brew '{create_air()}' "
+      f"Recipe transmuting Lead to Gold: brew '{create_air()}' "
       f"and '{strength_potion()}' mixed with "
       f"'{elements.create_fire()}'"
     )
